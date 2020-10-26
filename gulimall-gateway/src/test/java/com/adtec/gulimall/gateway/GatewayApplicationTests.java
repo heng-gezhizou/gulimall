2ba43a044ceb4e74adb82bac44206f5682d2dd25
hgzz <1873477865@qq.com>
2020-10-26 20:17:15 +0800

    2020/10/26 gateway

---
+++ gulimall-gateway/src/test/java/com/adtec/gulimall/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adtec.gulimall.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
