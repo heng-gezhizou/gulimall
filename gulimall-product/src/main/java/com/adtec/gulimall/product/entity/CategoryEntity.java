215379b6a6cd2cc1a868ec2de8c539d3d8f176ff
hgzz <1873477865@qq.com>
2020-12-02 20:56:11 +0800

    2020/12/2

--- gulimall-product/src/main/java/com/adtec/gulimall/product/entity/CategoryEntity.java
+++ gulimall-product/src/main/java/com/adtec/gulimall/product/entity/CategoryEntity.java
@@ -8,6 +8,7 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 /**
@@ -62,6 +63,7 @@ public class CategoryEntity implements Serializable {
 	/**
 	 * 子目录
 	 */
+	@JsonInclude(JsonInclude.Include.NON_EMPTY)
 	@TableField(exist=false)
 	private List<CategoryEntity> children;
 
